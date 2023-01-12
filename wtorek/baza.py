a8293726c831e70e826b1bc456ae1d0e5f65113d
Mulu123 <muluhabt.fentahun@orange.com>
2023-01-12 15:08:16 +0100

    added baza

---
+++ wtorek/baza.py
@@ -0,0 +1,56 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (
+    Column, Integer, String, ForeignKey, create_engine)
+from sqlalchemy.orm import relationship
+
+
+from sqlalchemy.orm import relationship
+
+engine = create_engine('sqlite:///memory.db')
+Base = declarative_base()
+
+
+class Person(Base):
+    __tablename__ = 'person'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    addresses = relationship('Address',
+                             back_populates='person',
+                             order_by="Address.email",
+                             cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f'{self.name}(id={self.id})'
+
+
+class Address(Base):
+    __tablename__ = 'address'
+    id = Column(Integer, primary_key=True)
+    email = Column(String)
+    person_id = Column(ForeignKey('person.id'))
+    person = relationship('Person', back_populates='addresses')
+
+    def __str__(self):
+        return self.email
+
+    __repr__ = __str__
+
+
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+anakin = Person(name='Anakin Skywalker', age=32)
+obi1 = Person(name='Obi-Wan Kenobi', age=40)
+obi1.addresses = [
+    Address(email='obi1@example.com'),
+    Address(email='wanwan@example.com'),
+]
+session.add(anakin)
+session.add(obi1)
+session.commit()
+
+obi1 = session.query(Person).filter(
+    Person.name.like('Obi%')
+).first()
+print(obi1, obi1.addresses)
